d5dd5f8bf1d0b2f2f9066671900ea155630b1496
Malu Repossi <106559836+malu-repossi@users.noreply.github.com>
2023-04-06 16:25:53 -0300

    Create busca sequencial com sentinela.c

---
+++ busca sequencial com sentinela.c
@@ -0,0 +1,68 @@
+/*
+Esse código apresenta o uso da Busca Sequencial ao procurar uma chave dentro de uma tabela desordenada com o 
+auxílio de um sentinela.
+Seu objetivo é retornar os registros da chave encontrada.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#define tamanho 3
+
+typedef struct Registro
+{
+    int matricula, quantFaltas;
+    char nome[50];
+    float notaFinal;
+} registro;
+
+int pesquisa(registro *tabela, int chave);
+
+
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+
+    int i, id, retorno;
+    registro alunos[tamanho];
+
+    for (i = 0; i < tamanho; i++){
+        printf("Digite a matrícula do aluno %d:\n", i+1);
+        scanf("%d", &alunos[i].matricula);
+        printf("Digite o nome do aluno %d:\n", i+1);
+        scanf("%s", alunos[i].nome);
+        printf("Digite a quantidade de faltas do aluno %d:\n", i+1);
+        scanf("%d", &alunos[i].quantFaltas);
+        printf("Digite a nota final do aluno %d:\n", i+1);
+        scanf("%f", &alunos[i].notaFinal);
+    }
+
+    printf("\n");
+    printf("Digite a matrícula do aluno que deseja encontrar:\n");
+    scanf("%d", &id);
+
+    retorno = pesquisa(alunos, id);
+
+    if(retorno == -1){
+         printf("\nAluno não encontrado! \n");
+    }
+    else{
+        printf("\n");
+            printf("Nome: %s \n", alunos[retorno].nome);
+            printf("Nota Final: %.2f \n", alunos[retorno].notaFinal);
+            printf("Quantidade de faltas: %d \n", alunos[retorno].quantFaltas);
+    }
+
+    return 0;
+}
+
+int pesquisa(registro *tabela, int chave){
+    int i;
+    tabela[tamanho].matricula = chave;
+
+    for (i = 0; chave != tabela[i].matricula; i++);
+    
+    if(i < tamanho) 
+        return i;
+    else
+        return -1;
+}
