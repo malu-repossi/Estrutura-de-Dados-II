0b12dd075809eb88737df1a9a8784dce99f9eeb7
Malu Repossi <106559836+malu-repossi@users.noreply.github.com>
2023-04-06 16:27:41 -0300

    Create busca sequencial - mover para frente.c

---
+++ busca sequencial - mover para frente.c
@@ -0,0 +1,82 @@
+/*
+Esse código apresenta o uso da Busca Sequencial ao procurar uma chave dentro de uma tabela desordenada.
+Seu objetivo é retornar os registros da chave encontrada e mover para frente (para o primeiro índice).
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#define tamanho 3
+
+typedef struct Registro
+{
+    int matricula, quantFaltas;
+    char nome[50];
+    float notaFinal;
+} registro;
+
+int pesquisa(registro *tabela, int chave);
+
+
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+
+    int i, id, retorno;
+    registro alunos[tamanho];
+
+    for (i = 0; i < tamanho; i++){
+        printf("Digite a matrícula do aluno %d:\n", i+1);
+        scanf("%d", &alunos[i].matricula);
+        printf("Digite o nome do aluno %d:\n", i+1);
+        scanf("%s", alunos[i].nome);
+        printf("Digite a quantidade de faltas do aluno %d:\n", i+1);
+        scanf("%d", &alunos[i].quantFaltas);
+        printf("Digite a nota final do aluno %d:\n", i+1);
+        scanf("%f", &alunos[i].notaFinal);
+    }
+
+    printf("\n");
+    printf("Digite a matrícula do aluno que deseja encontrar:\n");
+    scanf("%d", &id);
+
+    retorno = pesquisa(alunos, id);
+
+    if(retorno == -1){
+         printf("\nAluno não encontrado! \n\n");
+    }
+    else{
+        printf("\n");
+            printf("Nome: %s \n", alunos[retorno].nome);
+            printf("Nota Final: %.2f \n", alunos[retorno].notaFinal);
+            printf("Quantidade de faltas: %d \n\n", alunos[retorno].quantFaltas);
+    }
+
+    printf("Tabela dos alunos matriculados:\n");
+    for (i = 0; i < tamanho; i++){
+        printf("Nome: %s \n", alunos[i].nome);
+        printf("Nota Final: %.2f \n", alunos[i].notaFinal);
+        printf("Quantidade de faltas: %d \n", alunos[i].quantFaltas);
+        printf("\n");
+    }
+
+    return 0;
+}
+
+int pesquisa(registro *tabela, int chave){
+    int i, j;
+    registro auxiliar;
+
+    for (i = 0; i < tamanho; i++){
+        if(tabela[i].matricula == chave){
+            auxiliar = tabela[i];
+            for(j = i; j > 0; j--){
+                tabela[j] = tabela[j-1];
+            }
+            tabela[0] = auxiliar;
+
+            return 0;
+        }
+    }
+
+    return -1;
+}
